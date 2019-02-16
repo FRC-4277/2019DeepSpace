76ca0d27480ce704d00d87b361f1bfe0be0343de
NoNameSteve <cooperRoboticsBooster@gmail.com>
2019-02-16 09:37:36 -0600

    Move NavX reset to auto init (previously on teleop init)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -192,6 +192,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    navX.reset();
     instansiateSubsystems();
     m_autonomousCommand = m_chooser.getSelected();
 
@@ -219,7 +220,6 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     instansiateSubsystems();
-    navX.reset();
     // Switch tab to General
     Shuffleboard.selectTab("General");
     // This makes sure that the autonomous stops running when
