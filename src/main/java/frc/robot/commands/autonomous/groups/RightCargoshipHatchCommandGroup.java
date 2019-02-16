2901b183bfeb4522434a974b1a1b8a61984bb73d
NoNameSteve <cooperRoboticsBooster@gmail.com>
2019-02-16 15:06:29 -0600

    Increase distance of autonomous by 4 inches (.3 feet)

--- src/main/java/frc/robot/commands/autonomous/groups/RightCargoshipHatchCommandGroup.java
+++ src/main/java/frc/robot/commands/autonomous/groups/RightCargoshipHatchCommandGroup.java
@@ -16,7 +16,7 @@ public class RightCargoshipHatchCommandGroup extends CommandGroup {
    * Add your docs here.
    */
   public RightCargoshipHatchCommandGroup() {
-    addSequential(new DriveToCommand(0.0, 12.0, 0.0, 5.1));
+    addSequential(new DriveToCommand(0.0, 12.3, 0.0, 5.3));
     addSequential(new DriveStopOnLineCommand(0.4, "left"));
     addSequential(new HatchRocketPlaceGroup());
     // Add Commands here:
